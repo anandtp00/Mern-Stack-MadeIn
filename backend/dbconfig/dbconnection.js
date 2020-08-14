9d9f82117dbfb48cace447ed6e7eb91d9c7196c0
anandtp00 <anandtp00@gmail.com>
2020-08-15 00:18:45 +0530

    mongo db connection

--- backend/dbconfig/dbconnection.js
+++ backend/dbconfig/dbconnection.js
@@ -4,7 +4,7 @@ const user = process.env.DBUSER;
 const pass = process.env.DBPASS;
 const database = process.env.DB;
 
-const url = `mongodb+srv://${user}:${pass}@madein.bf8ty.gcp.mongodb.net/${database}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${user}:${pass}@madein.hx4wi.mongodb.net/${database}?retryWrites=true&w=majority`;
 const mongoConnect = mongoose.connect(process.env.MONGODB_URI || url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (error, success) => {
     if (error) {
         console.log('Error in DB connection : ' + JSON.stringify(error, undefined, 2));
